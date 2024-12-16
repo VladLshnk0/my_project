fea7a5530fb61dcc254a647abf3d05b133e9e1f9
Vlad <vlad.lshnk0@gmail.com>
2024-12-16 13:38:14 +0100

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
             </code>
             .
           </li>
-          <li>Save and see your changes instantly.</li>
+          <li>bla bla bla</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
